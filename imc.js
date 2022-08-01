25b23ad057f5919fd0c5f47eb01c0d6685129454
Andreia Alves da Costa <andreia@network.local>
2022-08-01 12:27:48 -0300

    Project finalized v1

--- imc.js
+++ imc.js
@@ -1,11 +1,9 @@
-
 const submit = document.getElementById('submit');
-
 
 function imc () {
     const name = document.getElementById('name').value;
-    const weight = document.getElementById('weight').value;
-    const height = parseFloat(document.getElementById('height').value).toFixed(2);
+    const weight = parseInt(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
     const results = document.getElementById('results');
 
     if (name === '') {
@@ -21,13 +19,22 @@ function imc () {
     }
 
     else {
+        let imcV = weight / Math.pow(height,2)
 
-        let imcV = (weight / Math.pow(height,2))*10000
-        if (imcV >=10) {
-            results.textContent = `${name} your IMC is ${imcV.toFixed(2)}. You're good!`
+        if (imcV < 18.50) {
+            results.textContent = `${name}, your BMI is ${imcV.toFixed(2)}. You're underweight!`
         }
-        else {
-        alert("Too bad")
+        else if (imcV <= 24.99) {
+            results.textContent = `${name}, your BMI is ${imcV.toFixed(2)}. You're good!`
+        }
+        else if (imcV <= 29.99) {
+            results.textContent = `${name}, your BMI is ${imcV.toFixed(2)}. You're overweight!`
+        }
+        else if (imcV <= 34.99) {
+            results.textContent = `${name}, your BMI is ${imcV.toFixed(2)}. You're obese!`
+        }
+        else if (imcV > 35.00) {
+            results.textContent = `${name}, your BMI is ${imcV.toFixed(2)}. You're extremely obese!`
         }
     }
 }
